2464fe59a681f99114b208384b213f9649597733
Logan Nelson <Lnelson0915@gmail.com>
2025-07-16 09:13:00 -0700

    Add BookStoreDb class for database content
    Introduces the 'BookStoreDb' class that inherits from 'DbContext' to facilitate database operations using Entity Framework Core.
    Configures the class to connect to a SQL Server database with a specified connection string and includes necessary namespaces.

---
+++ BookstoreApp/Database/BookStoreDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreApp.Database;
+
+public class BookStoreDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+            optionsBuilder.UseSqlServer(@"Server=(localdb\mssqllocaldb);Database=BookStoreDb;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
