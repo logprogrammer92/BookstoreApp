5b2e808d8d8641d54f3d81d7a5220f1f6f9680f7
logprogrammer92 <lnelson0915@gmail.com>
2025-07-16 15:25:41 -0700

    Issue#1 configure entity framework (#2)
    * Add Entity Framework Core packages to project
    
    Added a new `<ItemGroup>` in the project file to include
    package references for `Microsoft.EntityFrameworkCore.SqlServer`
    and `Microsoft.EntityFrameworkCore.Tools`, both at version 9.0.7.
    The `Microsoft.EntityFrameworkCore.Tools` package includes
    properties for managing asset visibility during build and runtime.
    
    * Add BookStoreDb class for database content
    
    Introduces the 'BookStoreDb' class that inherits from 'DbContext' to facilitate database operations using Entity Framework Core.
    Configures the class to connect to a SQL Server database with a specified connection string and includes necessary namespaces.
    
    * Add Book class to represent individual books for sale
    
    Introduces the `Book` class in the `BookstoreApp` namespace with properties for the book's unique identifier (`Id`), title (`Title`), and sales price (`Price`). The'Id' is marked with a `[Key]` attribute, and the 'Title' is required. Additional using directives are included.
    
    * Add DbSet<Book> to BookStoreDb for entity tracking
    
    This commit introduces a new property `Books` of type `DbSet<Book>` in the `BookStoreDb` class. This addition enables the application to track `Book` entities in the database, facilitating CRUD operations on the `Books` table.
    
    * Fixed the connection string in the db context class
    
    Revised the connection string in the `OnConfiguring` method
    of the `BookStoreDb` class to use `Data Source=(localdb)\\MSSQLLocalDB`
    and `Initial Catalog=BookstoreDb` with `Integrated Security=True`. Removed `Trusted_Connection=True` and added
    `Trust Server Certificate=False` for improved clarity and security.
    
    * Created a code migration for BookStore.Db

---
+++ BookstoreApp/Database/BookStoreDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreApp.Database;
+
+public class BookStoreDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookstoreDb;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;");
+    }
+
+    // Add entities to track in the database as DbSets below
+
+    public DbSet<Book> Books { get; set; }
+}
---
+++ BookstoreApp/Migrations/20250716201040_AddedBook.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookstoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
